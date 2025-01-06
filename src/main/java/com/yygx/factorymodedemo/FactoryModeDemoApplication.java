9b7fdc3cb178c7e6534cf24c3a1331751c31e1e9
吟风抱月 <3472151649.@qq.com>
2025-01-06 18:42:42 +0800

    工厂模式demo初始化

---
+++ src/main/java/com/yygx/factorymodedemo/FactoryModeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yygx.factorymodedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryModeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FactoryModeDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yygx/factorymodedemo/FactoryModeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yygx.factorymodedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactoryModeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
